78b0876918702ffca7297a48932757f911ae0dc8
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-22 20:40:46 +0200

    complete snack 05

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let evenNumbers = [];
+
+nums.forEach((num) => (num % 2 === 0 ? evenNumbers.push(num) : false));
+
+console.log(evenNumbers);
