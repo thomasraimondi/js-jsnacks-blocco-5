d5ec702587906591d2c70bc8eb978bb081874252
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-22 21:15:21 +0200

    complete snack 08

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,31 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classesStudent = students.map((student) =>
+  student.name === "Marco Lanci" ? student.class : false
+);
+
+const classStudent = classesStudent.filter((clas) => clas !== false);
+
+console.log(classStudent);
